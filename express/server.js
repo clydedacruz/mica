cba81ed6d218b9be6b6cb729bdf5bac284f8e293
Clyde Dcruz <clyde_dcruz@persistent.co.in>
2019-03-24 18:01:37 +0530

    add cors config

--- express/server.js
+++ express/server.js
@@ -4,12 +4,21 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+function allowCrossDomain(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+}
+
 app.use(allowCrossDomain);
 
 // this uses the mobile app's URL scheme to redirect to the app on the mobile phone browser
